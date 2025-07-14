3d0eca8a2d7d54a009438dc18053150a2cba0730
Mintian Cui <104343925+BioinAI@users.noreply.github.com>
2025-07-14 21:47:11 +0800

    Update __init__.py

--- deepadvancer/__init__.py
+++ deepadvancer/__init__.py
@@ -1,6 +1,8 @@
 from .deepadvancer import (
-    load_and_process_expression_data, #整合数据
-    run_logfc_analysis_and_generate_fc_array, #重构数据
-    compute_logfc_between_classes, #比较类别之间
-    compute_logfc_vs_others #比较一个类与所有类
+    load_and_process_expression_data, 
+    run_logfc_analysis_and_generate_fc_array,
+    recon_training,
+    compute_logfc_between_classes,
+    compute_logfc_vs_others, 
+    plot_pca_by_class
 )
